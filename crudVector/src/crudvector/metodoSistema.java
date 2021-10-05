05dd4fccb270d52e442b3bee6cea1d55ab3478d2
ManuelCM21 <manuelchuncamamani@gmail.com>
2021-10-05 18:18:58 -0500

    CRUD con Vector

--- crudVector/src/crudvector/metodoSistema.java
+++ crudVector/src/crudvector/metodoSistema.java
@@ -11,25 +11,10 @@ public class metodoSistema {
         lista.add(x);
     }
     
-    public void eliminarPersona(Object x){
-//        if (y.equals(ve.nombres) || y.equals(ve.codigo)) {
-//            for(int i = 0; i<lista.size(); i++){
-//                filas[i][0]= ve.nombres;
-//                filas[i][1]= ve.ap_paterno;
-//                filas[i][2]= ve.ap_materno;
-//                filas[i][3]= ve.codigo;
-//            }
-//            lista.remove(filas);
-//        }
-        Object filas[][]=new Object[lista.size()][4];
-        for(int i = 0; i<lista.size(); i++){
+    public void eliminarPersona(String nombre){
+        for (int i = 0; i < lista.size(); i++) {
             datosPersona dp = (datosPersona)lista.get(i);
-            filas[i][0]= dp.nombres;
-            filas[i][1]= dp.ap_paterno;
-            filas[i][2]= dp.ap_materno;
-            filas[i][3]= dp.codigo;
-            if (x.equals(dp.nombres) || x.equals(dp.codigo)) {
-                System.out.println("Eliminando a: "+dp.nombres);
+            if (nombre.equals(dp.nombres)) {
                 lista.remove(i);
             }
         }
